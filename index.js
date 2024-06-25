0080e27988ad7e3fc653fdd5405c46f6284cc5dd
Joao Pedro Tanganeli <tanganelijoao@outlook.com>
2024-06-25 17:01:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const {Client} = require("discord.js");
+require("dotenv").config
+
+// const client = new Client({intents: []});
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi"){
+        message.reply("Hello World!")
+    }
+})
+
+
+client.login(process.env.TOKEN);
